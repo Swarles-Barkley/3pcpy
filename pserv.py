29cba45a9171267325adae19a28cd457fc7cac96
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-02-12 16:51:09 -0800

    set SO_REUSEADDR socket option
    If you have to kill and restart the server, this eliminates spurious
    "address already in use" errors.

--- pserv.py
+++ pserv.py
@@ -20,6 +20,7 @@ doCommits = [False,False,False]
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind((TCP_IP, TCP_PORT))
 sock.listen(5)
 
