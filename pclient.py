505f4a1e09e5ee5f9563879a8393389ccb57f54e
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-02-12 16:56:12 -0800

    remove dead code

--- pclient.py
+++ pclient.py
@@ -5,17 +5,14 @@ import hashlib
 import random
 import time
 import sys
+
 TCP_IP = '127.0.0.1'
 TCP_PORT = 1878
 BUFFER_SIZE = 1024
-MESSAGE = "startplease"
-time.sleep(5)
+#time.sleep(5)
 random.seed(1848)
 data = ""
-#sock.send(MESSAGE)
-#data = sock.recv(BUFFER_SIZE)
-#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#sock.connect((TCP_IP, TCP_PORT))
+
 resp = sys.argv[1]
 
 def log(*args):
