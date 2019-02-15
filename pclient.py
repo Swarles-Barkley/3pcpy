105f3fcc3a2e0f00335cdd0c4f4c9026b64299f7
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-02-14 18:36:32 -0800

    make number of nodes configurable

--- pclient.py
+++ pclient.py
@@ -13,43 +13,42 @@ BUFFER_SIZE = 1024
 random.seed(1848)
 data = ""
 
-resp = sys.argv[1]
+nodenum = sys.argv[1]
 
 def log(*args):
-    print("[client %s]" % resp, *args)
+    print("[client %s]" % nodenum, *args)
 
-sanity = 0
-while(data!="Yes" and sanity<30):
-    time.sleep(1)
+class Timeout(Exception):
+    """Request timed out"""
+
+def send(msg):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((TCP_IP, TCP_PORT))
-    sock.send("canCommit?\31"+resp)
-    data = sock.recv(BUFFER_SIZE)
-    sanity = sanity+1
-sanity = 0
-while(data!="ACK" and sanity<30):
-    time.sleep(1)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((TCP_IP, TCP_PORT))
-    sock.send("preCommit\31" + resp)
-    data = sock.recv(BUFFER_SIZE)
-    sanity = sanity+1
+    try:
+        sock.send(msg)
+        data = sock.recv(BUFFER_SIZE)
+        return data
+    finally:
+        sock.close()
+
+def trysend(msg):
+    for sanity in range(30):
+        data = send(msg)
+        if data and data != "Wait":
+            return data
+        time.sleep(1)
+
+    raise Timeout()
+
+def main():
+    data = trysend("hello\31"+nodenum)
+
+    data = trysend("canCommit?\31"+nodenum)
+
+    data = trysend("preCommit\31" + nodenum)
     log("received:", data)
-sanity = 0
-while(data!="haveCommitted" and sanity<30):
-    time.sleep(1)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((TCP_IP, TCP_PORT))
-    sock.send("doCommit\31" + resp)
-    data = sock.recv(BUFFER_SIZE)
-    sanity = sanity+1
+
+    data = trysend("doCommit\31" + nodenum)
     log("received:", data)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((TCP_IP, TCP_PORT))
-sock.send("doCommit\31" + resp)
-data = sock.recv(BUFFER_SIZE)
-log("received:", data)
-
-sock.close()
-
+main()
--- pserv.py
+++ pserv.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import socket
 import hashlib
 import time
@@ -9,18 +10,13 @@ import threading
 TCP_IP = '127.0.0.1'
 TCP_PORT = 1878
 BUFFER_SIZE = 1024
-oldtime = int(time.time())
-data_lock = threading.Lock()
+N = 3 # number of nodes
 
+data_lock = threading.Lock() # protects the following variables
+nodes = []
 canCommits = [False,False,False]
 preCommits = [False,False,False]
 doCommits = [False,False,False]
-
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind((TCP_IP, TCP_PORT))
-sock.listen(5)
 
 def threadConn(conn):
     data = conn.recv(BUFFER_SIZE)
@@ -31,8 +27,18 @@ def threadConn(conn):
     #data = data[5:]
     print "data is " + data
 
+    if cmd == "hello":
+        with data_lock:
+            nodes.append(nodenum)
+            if len(nodes) < N:
+                conn.send("Wait")
+            else:
+                conn.send("Start")
+                canCommits[:] = [False]*N
+                preCommits[:] = [False]*N
+                doCommits[:] = [False]*N
 
-    if cmd == "canCommit?":
+    elif cmd == "canCommit?":
         with data_lock:
             canCommits[nodenum] = True
             if not all(canCommits):
@@ -59,9 +65,17 @@ def threadConn(conn):
     else:
         print("Server doesnt understand: " + data)
 
-    print "server received: ", data
-
 def main():
+    global N
+
+    if len(sys.argv) >= 2:
+        N = int(sys.argv[1])
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((TCP_IP, TCP_PORT))
+    sock.listen(5)
+
     while 1:
         conn, addr = sock.accept()
         thr = threading.Thread(target=threadConn, args=(conn,))
