757335589e75d5895c7e96e965e086c055b19581
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-03-15 15:00:52 -0700

    topology trials

--- trials.sh
+++ trials.sh
@@ -2,10 +2,10 @@ set -eu
 trials=100
 run=1
 seed=1234
-mkdir -p logs/g2/
-for failure in 05 10 15 25 20 30; do
+mkdir -p logs/topo1/
+for failure in 05 10 15 20 25 30; do
     for i in {1..$trials}; do
-        nodes=25 zsh runner.sh --seed=$seed --quorum=0.96 --failure=0.$failure |& tee logs/g2/run-$run-q96-f$failure-r3-trial$i.log
+        nodes=25 zsh runner.sh --topology --topology-seed=$i --seed=$seed --quorum=0.96 --failure=0.$failure |& tee logs/topo1/run-$run-q96-f$failure-r3-topo$i-trial$i.log
         ((seed+=25))
     done
     ((run += 1))
@@ -15,4 +15,8 @@ done
 # gather time, bytes, success rate
 
 # vary nodes, 
-# 
+
+# topology
+# very difficult
+# fix failure to something reasonable
+# vary quorum
