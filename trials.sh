e35cae40c116cc48bfafd3d4a78f18a11b7db664
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-03-15 18:10:41 -0700

    connected trial with varying failure

--- trials.sh
+++ trials.sh
@@ -2,11 +2,12 @@ set -eu
 trials=100
 run=1
 seed=1234
-mkdir -p logs/topo2/
+dir=logs/g3
+mkdir -p "$dir"
 failure=0.15
 for quorum in 1.00 .90 .80 .70 .60; do
     for i in {1..$trials}; do
-        nodes=25 zsh runner.sh --topology --topology-seed=$i --seed=$seed --quorum=$quorum --failure=$failure |& tee logs/topo2/run-$run-q${quorum/./}-f${failure/./}-r3-topo$i-trial$i.log
+        nodes=25 zsh runner.sh --seed=$seed --quorum=$quorum --failure=$failure |& tee "$dir"/run-$run-q${quorum/./}-f${failure/./}-r3-trial$i.log
         ((seed+=25))
     done
     ((run += 1))
